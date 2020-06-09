8891dfe74208126d0c74482fce46d3a1096cd06a
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-09 16:30:42 -0500

    holy hell i fixed it

--- scripts/parks/parkList.js
+++ scripts/parks/parkList.js
@@ -1,6 +1,6 @@
 //function to show park in dropdown
 const parkList = () => {
-    for (const currentParkObject of parkCollection) {
+    for (const currentParkObject of parkCollection.data) {
     const parkHTML = parkConverter(currentParkObject);
     const parkSelectElement = document.querySelector(".parkChoice");
     parkSelectElement.innerHTML += parkHTML;
