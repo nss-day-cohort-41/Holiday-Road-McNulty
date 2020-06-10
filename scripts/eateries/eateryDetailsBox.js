6c72c96d1e34aad5f9ab16772449185225eae4fe
TriggSumms <stsumms@gmail.com>
2020-06-10 14:01:58 -0500

    Triggs Edits

--- scripts/eateries/eateryDetailsBox.js
+++ scripts/eateries/eateryDetailsBox.js
@@ -1,9 +1,30 @@
 const eateryConverter = (eateryObject) => {
-    const eateryHTMLRepresentation =  `
+    const eateryHTML =  `
     
         <option class="eateryOption">${eateryObject.businessName}</option>
     
     `
-    // , ${eateryObject.state}
-    return eateryHTMLRepresentation;
+   
+    return eateryHTML;
 }
+
+
+
+
+
+
+// Converter to send in new information to the preview dropdown
+const EateryPreviewConverter = (eateryCollection, .businessName) =>{
+    const eateryPreviewHTML =
+`
+    
+<ul>
+<li>${eateryCollection.buisnessName}</li>
+<li> </li>
+</ul>
+
+`
+ return eateryPreviewHTML;
+}
+
+
