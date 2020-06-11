c00b6dcc89dcdfff46a71308107e0b7610999b5b
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-11 13:42:13 -0500

    continuning to figure out saving

--- scripts/itinerary.js
+++ scripts/itinerary.js
@@ -1,5 +1,21 @@
 const saveItineraryButton = document.querySelector(".submitButton")
 
+//responsible for creating save button functionality
 const saveYourItinerary = () => {
+    //upon clicking save button
+    saveItineraryButton.addEventListener("click", clickEvent => {
+    // will check to see if one of each selection has been made. If so, save will happen.
+    // need to change these variables once Trigg gets them assigned
+     if (parkPreview === true && eateryPreview === true && bizarrePreview === true)
+    //save itinerary
+      {
 
+      }
+    // if missing any of the selections, alert will prompt to let user know
+     else (alert("Please select an option from each menu"))
+      }
+    )
 }
+
+// this is where I want the data to go
+// "http://localhost:8088/itineraries"
