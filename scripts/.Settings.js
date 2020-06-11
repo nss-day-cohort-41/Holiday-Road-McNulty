51eccf4e16cfe5aecaaedd5e99ad314e707657ec
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-11 10:10:28 -0500

    deleted nefarious settings file

--- scripts/.Settings.js
+++
@@ -1,5 +0,0 @@
-export default {
-	// graphhopperKey: "F4e3aba9-94a1-4472-95d7-d927333cea98 ",
-	npsKey: "vw3gSXfcGrhrPdn1uVdQBfCAqmTtbaL1VsFVDbNn",
-	weatherKey: "1f6ff824785b42bf208a5173b245ee80"
-}
