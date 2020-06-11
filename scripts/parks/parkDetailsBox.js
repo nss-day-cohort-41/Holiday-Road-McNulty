7ad193e8b8cd8fcafbea3d5c3ede966e74679c61
Brandon Wheatley <brandon.a.wheatley@gmail.com>
2020-06-11 10:03:54 -0500

    removed some dead code

--- scripts/parks/parkDetailsBox.js
+++ scripts/parks/parkDetailsBox.js
@@ -4,16 +4,3 @@ const parkConverter = (parkObject) => {
     `
     return parkHTMLRepresentation;
 }
-
-// trying to get this to work please kill me 
-// const parkDetailsConverter = (parkObject) => {
-//     const parkDetailsHTML = `
-//     Park Name: ${parkObject.data.name}
-//     Address: ${parkObject.data.addresses[0].line1} ${parkObject.data.addresses[0].city} ${parkObject.data.addresses[0].stateCode} ${parkObject.data.addresses[0].postalCode}
-//     Entrance Fee: ${parkObject.data.entraceFees[0].cost}
-//     Description: ${parkObject.data.description}
-//     `
-//     return parkDetailsHTML
-// }
-
-// console.log(parkDetailsConverter)
